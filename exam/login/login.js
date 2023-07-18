c12101bc40c74d14c81c84df6292c321ef3e8253
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2023-07-18 19:59:13 +0330

    finish

--- exam/login/login.js
+++ exam/login/login.js
@@ -1,20 +1,20 @@
-let retrievedObjectLG = localStorage.getItem("users");
-retrievedObjectLG = JSON.parse(retrievedObjectLG);
-let EILG = document.getElementById("EI");
-let PILG = document.getElementById("PI");
+
+let retrievedObjectLG = localStorage.getItem('users');
+retrievedObjectLG = JSON.parse(retrievedObjectLG)
+let EILG = document.getElementById("EI")
+let PILG = document.getElementById("PI")
 
 function checkuser() {
-  for (let index = 0; index < retrievedObjectLG.length; index++) {
-    if (retrievedObjectLG[index]["email"] == EILG.value) {
-      let userLG = retrievedObjectLG[index];
-      if (userLG["password"] == PILG.value) {
-        SetAction("Login", "yes");
-        alert("User Login Success !");
-        window.location = "http://127.0.0.1:5500";
-      } else {
-        SetAction("Login", "No");
-        alert("User Login Error !");
-      }
+    for (let index = 0; index < retrievedObjectLG.length; index++) {
+        if (retrievedObjectLG[index]['email'] == EILG.value && retrievedObjectLG[index]['password'] == PILG.value) {
+            SetAction("Login","yes")
+            alert("User Login Success !")
+            window.location = "http://127.0.0.1:5500/exam/index.html"
+            break
+        }else{
+            SetAction("Login","yes")
+            alert("User Login not success !")
+            break
+        }
     }
-  }
 }
